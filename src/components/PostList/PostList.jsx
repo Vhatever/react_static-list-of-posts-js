18c9d065a0cf55bf7f68e697e0dbcca856d53a4e
Vhatever <fserrorka@gmail.com>
2025-08-25 15:10:46 +0300

    solution100%

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,9 +1,9 @@
 import { PostInfo } from '../PostInfo/PostInfo';
 import './PostList.scss';
 
-export const PostList = ({ ports }) => (
-  <div>
-    {ports.map(post => (
+export const PostList = ({ posts }) => (
+  <div className="PostList">
+    {posts.map(post => (
       <PostInfo key={post.id} post={post} />
     ))}
   </div>
